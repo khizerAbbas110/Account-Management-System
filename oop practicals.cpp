cf626e5b363abd5559d2da22117e1af029cb7abf
KHIZER ABBAS <khizerjaffri46@gmail.com>
2025-08-29 10:47:31 -0700

    Add files via upload

---
+++ oop practicals.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+class Account {
+    int accountNo;
+    string holderName;
+    double balance;
+public:
+    
+    Account(int accNo, string name, double bal) 
+        : accountNo(accNo), holderName(name), balance(bal) {}
+
+    void deposit(double amount) {
+        balance += amount;
+    }
+
+    void withdraw(double amount) {
+        if (amount <= balance) {
+            balance -= amount;
+        } else {
+            cout << " Transaction failed: Insufficient Balance!" << endl;
+        }
+    }
+
+    void display() {
+        cout << "-----------------------------" << endl;
+        cout << "Account No : " << accountNo << endl;
+        cout << "Holder     : " << holderName << endl;
+        cout << "Balance    : " << balance << endl;
+        cout << "-----------------------------" << endl;
+    }
+};
+
+int main() {
+    Account a1(450, "Hassan", 7500);  
+    a1.display();
+
+    a1.deposit(1500);
+    a1.withdraw(500);
+
+    a1.display();
+
+    return 0;
+}
+
